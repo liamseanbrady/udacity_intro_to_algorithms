74818149e15c970a1fabc56d7d4504bc00e5f2ef
Liam Sean Brady <liamseanbrady@gmail.com>
2014-09-16 22:38:22 +0100

    Add Dijkstra's Shohortest Path algorithm done in Python

---
+++ dijkstra_shortest_path.py
@@ -0,0 +1,16 @@
+def dijkstra(G,v):
+    dist_so_far = {}
+    dist_so_far[v] = 0
+    final_dist = {}
+    while len(final_dist) < len(G):
+        w = shortest_dist_node(dist_so_far)
+        # lock it down!                                                                                                                                                                                     
+        final_dist[w] = dist_so_far[w]
+        del dist_so_far[w]
+        for x in G[w]:
+            if x not in final_dist:
+                if x not in dist_so_far:
+                    dist_so_far[x] = final_dist[w] + G[w][x]
+                elif final_dist[w] + G[w][x] < dist_so_far[x]:
+                    dist_so_far[x] = final_dist[w] + G[w][x]
+    return final_dist
